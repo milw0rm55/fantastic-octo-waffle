d9165779775e530649b62b6a033708fec2ef8f44
SEBASTIÁN TANGARIFE ACERO <sebastian.tangarife@utmac0017.local>
2018-10-24 12:08:41 +0200

    cells

---
+++ settings/SettingsViewController/Settings.swift
@@ -0,0 +1,9 @@
+//
+//  Settings.swift
+//  settings
+//
+//  Created by SEBASTIÁN TANGARIFE ACERO on 24/10/18.
+//  Copyright © 2018 SEBASTIÁN TANGARIFE ACERO. All rights reserved.
+//
+
+import Foundation
